f1f6e5edeaa558bc9e53493e870f86db8532e2b2
rl <74465003+ramone731@users.noreply.github.com>
2024-02-14 18:07:22 -0500

    Update EmployeeRepository.java

--- SpringWeb/src/main/java/com/example/demo/model/EmployeeRepository.java
+++ SpringWeb/src/main/java/com/example/demo/model/EmployeeRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.model;
 
-public class EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer>{
+
 }
